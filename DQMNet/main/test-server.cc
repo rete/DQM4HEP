fdc4b7d69dc9fafa8bb969d4a0fa9b26f60558a6
Ete Remi <remi.ete@desy.de>
2018-08-24 19:04:25 +0200

    Modified test server to test the new networking features

--- DQMNet/main/test-server.cc
+++ DQMNet/main/test-server.cc
@@ -8,57 +8,62 @@
 // Author     : R.Ete
 //====================================================================
 
-#include "dqm4hep/Server.h"
-#include "dqm4hep/Service.h"
+#include <dqm4hep/NetworkEventLoop.h>
+#include <dqm4hep/Logging.h>
+#include <dqm4hep/ServerConnection.h>
 
 using namespace dqm4hep::net;
 
-class MyPrintClass {
-public:
-  void print(const Buffer &request, Buffer &response) {
-    std::string requestStr(request.begin(), request.size());
-    std::cout << "Received : " << requestStr << std::endl;
-
-    std::string responseStr = "Hello world !";
-    std::cout << "Sending : " << responseStr << std::endl;
-
-    auto model = response.createModel<std::string>();
-    model->move(std::move(responseStr));
-    response.setModel(model);
-  }
-
-  void printCommand(const Buffer &command) {
-    std::string commandStr(command.begin(), command.size());
-    std::cout << "Received command : " << commandStr << std::endl;
-  }
-};
-
-int main(int /*argc*/, char ** /*argv*/) {
-  MyPrintClass printer;
-
-  Server *pServer = new Server("TestServer");
-
-  Service *pIntService = pServer->createService("/test/int");
-  Service *pFloatService = pServer->createService("/test/float");
-  pServer->createRequestHandler("/test/print", &printer, &MyPrintClass::print);
-  pServer->createCommandHandler("/test/printCommand", &printer, &MyPrintClass::printCommand);
-
-  pServer->start();
-
-  int intVal = rand();
-  float floatVal = intVal * 0.78;
-
-  while (1) {
-    intVal = rand();
-    std::cout << "Sending int = " << intVal << std::endl;
-    pIntService->send(intVal);
-
-    floatVal = intVal * 0.78;
-    std::cout << "Sending float = " << floatVal << std::endl;
-    pFloatService->send(floatVal);
-
-    sleep(5);
-  }
-
+int main(int argc, char ** argv) {
+  
+  NetworkEventLoop eventLoop;
+  ServerConnection connection(&eventLoop);
+  
+  connection.onHttpRequest([](const HttpMessage& message, HttpResponse& response){
+    dqm_info( "---------------------------" );
+    dqm_info( "-- Received HTTP request --" );
+    dqm_info( "---------------------------" );    
+    dqm_info( "-- Method: {0}", message.method() );
+    dqm_info( "-- Protocol: {0}", message.protocol() );
+    dqm_info( "-- URI: {0}", message.uri() );
+    dqm_info( "-- Query string: {0}", message.queryString().str() );
+    dqm_info( "-- Body: " );
+    dqm_info( std::string(message.body(), message.bodySize()) );
+    dqm_info( "---------------------------" );
+    response.response().append("Hello world !"); 
+    dqm_info( "-- Sending back '{0}'", response.response() );
+    dqm_info( "---------------------------" );
+  });
+  
+  connection.onNewConnection([](const Connection& con) {
+    dqm_info( "-- New connection, uri: {0}", con.uri() );
+  });
+  
+  connection.onConnectionClose([](const Connection& con) {
+    dqm_info( "-- Closing connection, uri: {0}", con.uri() );
+  });
+  
+  connection.onMessage([&eventLoop,&connection](const Connection& con, const WebsocketMessage& message) {
+    dqm_info( "-- Received message, uri: {0}", con.uri() );
+    std::string inmsg(message.message(), message.length());
+    dqm_info( "-- Message: {0}", inmsg );
+    if(inmsg == "stop") {
+      connection.stop();
+      eventLoop.stop();
+    }
+    else {
+      connection.send(con, "Boubouuuuuuuu !");
+      connection.close(con);      
+    }
+  });
+  
+  ServerConnection::BindConfig config;
+  config.m_port = argc > 1 ? atoi(argv[1]) : 8000;
+  config.m_enableWebsockets = true;
+  config.m_enableHttpRequests = true;
+  
+  connection.bind(config);
+  eventLoop.start(true, 200);
+  
   return 0;
 }
