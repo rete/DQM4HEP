b0bf89474f4dd99a3a38c086b47842ee212a874e
Remi Ete <remi.ete@gmail.com>
2018-08-27 03:20:43 +0200

    Added doxygen documentation

--- DQMNet/include/dqm4hep/Connection.h
+++ DQMNet/include/dqm4hep/Connection.h
@@ -11,43 +11,71 @@
 #ifndef DQM4HEP_CONNECTION_H
 #define DQM4HEP_CONNECTION_H
 
+// -- mongoose headers
 #include <mongoose.h>
 
 namespace dqm4hep {
   
   namespace net {
     
+    /**
+     *  @brief  Connection class
+     *  Wraps a mongoose connection and provides custom accessors
+     */
     class Connection {
     public:
+      /**
+       *  @brief  Constructor
+       *  
+       *  @param  route the connection route, e.g '/list'
+       *  @param  connection the connection implementation (mongoose)
+       */
       inline Connection(const std::string& route, struct mg_connection *connection) :
         m_connection(connection),
         m_route(route) {
         
       }
       
+      /**
+       *  @brief  Get the socket fd of the connection
+       */
       inline sock_t socket() const {
         return m_connection->sock;
       }
       
+      /**
+       *  @brief  Get the manager that owns the connection
+       */
       inline mg_mgr *manager() const {
         return m_connection->mgr;
       }
       
+      /**
+       *  @brief  Get the connection route
+       */
       inline const std::string &route() const {
         return m_route;
       }
-      
+        
+      /**
+       *  @brief  Get the connection implementation
+       */
       inline struct mg_connection* connection() const {
         return m_connection;
       }
       
+      /**
+       *  @brief  Cast the connection user data to the specified type
+       */
       template <typename T>
       inline T* connectionAs() const {
         return (T*) m_connection->user_data;
       }
       
     private:
+      /// The connection implementation (mongoose)
       struct mg_connection*       m_connection = {nullptr};
+      /// The connection route
       std::string                 m_route = {};
     };
     
